8f8a5a52f3123a628c2d56a581eda829accf661e
aharalabMeiji <ahara@meiji.ac.jp>
2018-01-18 16:19:42 +0900

    前書き案を足しておきます。 CSVファイルを出力する部分を追加しておきます。

--- cubicalripser_3dim.cpp
+++ cubicalripser_3dim.cpp
@@ -1,3 +1,28 @@
+/*
+CubicalRipser: C++ system for computation of Cubical persistence pairs
+Copyright 2017-2018 Takeki Sudou and Kazushi Ahara.
+CubicalRipser is free software: you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+CubicalRipser is deeply depending on 'Ripser', software for Vietoris-Rips 
+persitence pairs by Ulrich Bauer, 2015-2016.  We appreciate Ulrich very much.
+We rearrange his codes of Ripser and add some new ideas for optimization on it 
+and modify it for calculation of a Cubical filtration.
+
+This part of CubicalRiper is a calculator of cubical persistence pairs for 
+2 dimensional pixel data. The input data format conforms to that of DIPHA.
+ See more descriptions in README.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 //#define PRINT_PERSISTENCE_PAIRS
 #define FILE_OUTPUT
 
@@ -783,6 +808,31 @@ int main(int argc, char** argv){
 		writing_file.write((char *) &writedeath, sizeof( double )); // death
 	}
 	writing_file.close();
+#endif
+
+#ifdef CSV_OUTPUT
+	string outname = output_filename;// .csv file
+	ofstream writing_file;
+	writing_file.open(outname, ios::out);
+
+	if(!writing_file.is_open()){
+		cout << " error: open file for output failed! " << endl;
+	}
+
+	int64_t p = writepairs.size();
+	for(int64_t i = 0; i < p; ++i){
+		writing_file << writepairs[i].getDimension() << ",";
+
+		writing_file << writepairs[i].getBirth() << ",";
+		writing_file << writepairs[i].getDeath() << endl;
+	}
+	writing_file.close();
+#endif
+
+#ifdef TIME_MEASURING
+	const auto endTime = chrono::system_clock::now();
+	const auto timeSpan = endTime - startTime;
+	cout << "processing-time : " << chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count() << "[ms]" << std::endl;
 #endif
 
 	return 0;
